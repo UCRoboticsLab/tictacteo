254e0eff07c0a9767d622c99cdad57bd35fabf3f
Robological <zhengzhi@robological.com>
2015-11-07 15:38:22 +1100

    will report when position reached

--- scripts/phm_game_controller.py
+++ scripts/phm_game_controller.py
@@ -55,6 +55,7 @@ class TigTagToe(object):
         self.GridCenter = []
         self.LeftArmInitPose = [0.5, 0.6, 0.0, 0.0, 1.0, 0.0, 0.0]
         self.RightArmInitPose = [0.5, -0.6, 0.0, 0.0, 1.0, 0.0, 0.0]
+        self.current_poses = None
         
     def get_vision_reply(self):    
         
@@ -102,10 +103,80 @@ class TigTagToe(object):
                       ',' + \
                       str(ow)
         self.ArmCmdPub.publish(msg_string)
+        print self.current_poses
+        cur_pose = self.current_poses[side]
+        dist_x = math.fabs(cur_pose.pose.position.x-x)
+        dist_y = math.fabs(cur_pose.pose.position.y-y)
+        dist_z = math.fabs(cur_pose.pose.position.z-z)
+        
+        while dist_x>0.002 and dist_y>0.02 and dist_z>0.02:
+            cur_pose = self.current_poses[side]
             
+            dist_x = math.fabs(cur_pose.pose.position.x-x)
+            dist_y = math.fabs(cur_pose.pose.position.y-y)
+            dist_z = math.fabs(cur_pose.pose.position.z-z)
+            rospy.sleep(0.1)
+        print "Postion Reached"
+    
+    def make_pose_stamp(self, pose_list, header):
+        
+        ps = PoseStamped()
+        ps.header = header
+        ps.pose.position = Point(x = pose_list[0], \
+                                 y = pose_list[1], \
+                                 z = pose_list[2], )
+
+        ps.pose.orientation = Quaternion(x = pose_list[3], \
+                                         y = pose_list[4], \
+                                         z = pose_list[5], \
+                                         w = pose_list[6], )
+        
+        return ps
+    
+    def init_msgs(self):
+        
+        left_arm_msg = message_filters.Subscriber("/robot/limb/left/endpoint_state",EndpointState)
+        right_arm_msg = message_filters.Subscriber("/robot/limb/right/endpoint_state",EndpointState)
+        ts = message_filters.ApproximateTimeSynchronizer([left_arm_msg, right_arm_msg], 10, 0.05)
+        ts.registerCallback(self.pose_callback)
+    
+    def pose_callback(self, left_msg, right_msg):
+    
+        pose1 = left_msg.pose
+        pose2 = right_msg.pose
+        
+        header = Header(stamp=rospy.Time.now(), frame_id='base')
+        cp1 = self.make_pose_stamp([pose1.position.x, \
+                                     pose1.position.y, \
+                                     pose1.position.z, \
+                                     pose1.orientation.x, \
+                                     pose1.orientation.y, \
+                                     pose1.orientation.z, \
+                                     pose1.orientation.w], \
+                                     header)
+        
+        cp2 = self.make_pose_stamp([pose2.position.x, \
+                                     pose2.position.y, \
+                                     pose2.position.z, \
+                                     pose2.orientation.x, \
+                                     pose2.orientation.y, \
+                                     pose2.orientation.z, \
+                                     pose2.orientation.w], \
+                                     header)
+        
+        self.current_poses = {'left':cp1, 'right':cp2}
+        
+        
+        #print self.current_poses
+        #print "\nleft: \n", cp1, "\n\nnew left: \n", cp2
+            
+        return    
         
     def init_game(self):
         
+        
+        rospy.sleep(1)
+        print "Game Init Starts"
         self.move_arm('left', self.LeftArmInitPose)
         rospy.sleep(2)
         self.move_arm('right', self.RightArmInitPose)
@@ -154,7 +225,8 @@ def main():
 
     signal.signal(signal.SIGINT, signal_handler)
     ttt_game = TigTagToe()
-        
+    ttt_game.init_msgs()
+    
     ttt_game.run()
     
 
